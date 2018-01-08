a1d510ee33441992a2abf220a46d14e78e10b5c4
churkinv <churkinv@gmail.com>
2018-01-08 22:22:28 +0200

    Basic Dictionary operations

---
+++ ConcurrentDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new Dictionary<string, int>()
+            {
+                {"Rick", 4},
+                {"Morty", 3}
+            };
+
+            Console.WriteLine($"No. of shirts in stock = {stock.Count}");
+
+            stock.Add("Let`sGetShifty", 6);
+            stock["BirdMan"] = 5;
+
+            stock["Let`sGetShifty"] = 12; // up from 6, 6 were bought
+            Console.WriteLine($"\r\nstock[Let`sGetShifty] = {stock["Let`sGetShifty"]}");
+
+            stock.Remove("Rick");
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (var kewValPair in stock)
+            {
+                Console.WriteLine($"{kewValPair.Key}: {kewValPair.Value}");
+            }
+            Console.WriteLine($"No. of shirts in stock = {stock.Count}");
+            Console.ReadKey();
+        }
+    }
+}
