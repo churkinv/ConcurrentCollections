5170700d94815c0737f9f90be7ec387b02cd1078
churkinv <churkinv@gmail.com>
2018-01-08 21:14:17 +0200

    The issue with multithreading

--- ConcurrentCollections/Program.cs
+++ ConcurrentCollections/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrentCollections
@@ -10,6 +11,29 @@ namespace ConcurrentCollections
     {
         static void Main(string[] args)
         {
+            var orders = new Queue<string>();
+            //PlaceOrders(orders, "Mark1");
+            //PlaceOrders(orders, "Sergii1");
+            Task task1 = Task.Run(() => PlaceOrders(orders, "Mark2"));
+            Task task2 = Task.Run(() => PlaceOrders(orders, "Sergii2"));
+            Task.WaitAll(task1, task2);
+
+            foreach (string order in orders)
+            {
+                Console.WriteLine("Order " + order);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void PlaceOrders(Queue<string> orders, string customerName)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1);
+                string orderName = string.Format($"{customerName}, {i + 1}");
+                orders.Enqueue(orderName);
+            }
         }
     }
 }
