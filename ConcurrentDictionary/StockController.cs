7dbefae1b75f8c9e168aa890152881efdc6ee883
vchurkin <ext_vchurkin@playtika.com>
2018-01-12 19:27:57 +0200

    DisplayStatus method added + comments

--- ConcurrentDictionary/StockController.cs
+++ ConcurrentDictionary/StockController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ConcurrentDictionary
 {
@@ -44,6 +40,10 @@ namespace ConcurrentDictionary
             return success;
         }
 
+        // Here we split one operation into two independent ones
+        // so we have simpler code
+        // but this method could have worse perfomance in some cases
+        // stock level could be negative
         public bool TrySellItem2(string item)
         {
             int newStockLevel = _stock.AddOrUpdate(item, -1, (key, oldValue) => oldValue - 1);
@@ -61,7 +61,24 @@ namespace ConcurrentDictionary
 
         internal void DisplayStatus()
         {
-            
+            int totalStock = _stock.Values.Sum();
+            System.Console.WriteLine("\r\nBought = " + _totalQuantityBought);
+            System.Console.WriteLine("Sold = " + _totalQuantitySold);
+            System.Console.WriteLine("Stock = " + totalStock);
+            int error = totalStock + _totalQuantitySold - _totalQuantityBought;
+            if (error == 0)
+                System.Console.WriteLine("Stock levels match");
+            else
+                System.Console.WriteLine("Error on stock level: " + error);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Stock level by item");
+            foreach (string itemName in Program.AllShirtNames)
+            {
+                int stockLevel = _stock.GetOrAdd(itemName, 0);
+                System.Console.WriteLine($"{itemName,-30}: {stockLevel}");
+            }
+
         }
     }
 }
